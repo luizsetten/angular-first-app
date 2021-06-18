e280b27a5b17fe0d6904326c1a3a029bdb947bf1
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-06-18 16:56:17 -0300

    feat: saving mocked product

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
@@ -8,13 +9,21 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   createProduct(): void {
-    this.productService.showMessage("Produto criado!")
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto criado!")
+      this.router.navigate(["products"])
+    })
   }
 
   cancel(): void {
