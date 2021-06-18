2b8e3cc9220611e9802c907a285f86e8d6d3ad7d
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-06-18 16:39:29 -0300

    feat: snack-bar

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,5 +1,6 @@
+import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  createProduct(): void {
+    this.productService.showMessage("Produto criado!")
+  }
+
+  cancel(): void {
+    this.router.navigate(["products"])
+  }
 }
