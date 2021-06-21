81dafce284f6c9ad77205674554d60d87697ff87
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-06-21 11:25:57 -0300

    feat: handle errors

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http'
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,16 +13,25 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string) {
+  showMessage(msg: string, isError: boolean = false) {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
 
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro', true)
+    return EMPTY
+  }
+
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 
   read(): Observable<Product[]> {
